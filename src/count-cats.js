06ee3fc569910d3cd94ac29b0040d8268794a0ae
ortophius@mail.ru <ortophius@mail.ru>
2020-09-18 02:39:47 +0400

    feat: implemented count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  if (!(backyard instanceof Array)) return 0;
+  let catsCount = 0;
+
+  backyard.forEach(row => {
+    if (!(row instanceof Array)) return;
+    row.forEach(cell => { if (cell === '^^') catsCount++ });
+  });
+
+  return catsCount;
 };
