e943c09e14c1ae6b6729e058517121a30e37fc86
ortophius@mail.ru <ortophius@mail.ru>
2020-09-18 02:41:18 +0400

    feat: implemented simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = ' ') {
+    this.chain.push('' + value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!position ||
+      !Number.isInteger(position) ||
+      position <= 0 ||
+      position > this.getLength()) {
+        this.chain = [];
+        throw new Error('Pls stop!');
+      };
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+    this.chain.forEach((link, i, chain) => {
+      str += '(';
+      str += (link === ' ') ? link : ` ${link} `;
+      str += ')';
+      if (i < chain.length - 1) str += '~~';
+    });
+    this.chain = [];
+    return str;
   }
 };
 
