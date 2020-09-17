b15a0ded08795614cbb1cbca756d9d96d5e35f2e
ortophius@mail.ru <ortophius@mail.ru>
2020-09-18 02:41:31 +0400

    feat: implemented transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const res = [];
+  const controls = ['--discard-next', '--double-next', '--discard-prev', '--double-prev'];
+  arr.forEach((item, i) => {
+    if (controls.indexOf(item) === -1) {
+      let itemCount = 1;
+
+      switch (arr[i - 1]) {
+        case controls[0]:
+          itemCount--;
+          break;
+        case controls[1]:
+          itemCount++;
+          break;
+      }
+
+      switch (arr[i + 1]) {
+        case controls[2]:
+          itemCount--;
+          break;
+        case controls[3]:
+          if (itemCount > 0) itemCount++;
+          break;
+      }
+
+      for (let i = 0; i < itemCount; i++) res.push(item);
+    }
+  });
+
+  return res;
 };
