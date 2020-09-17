628c55ce46935294d1bdabfb2998f58659c44132
ortophius@mail.ru <ortophius@mail.ru>
2020-09-18 02:42:15 +0400

    feat: implemented what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (date.toString.toString() !== Date.toString.toString()) throw new Error('Alert!');
+
+  const month = date.getMonth();
+  let monthName;
+  switch (true) {
+    case (month >= 2 && month <= 4):
+      monthName = 'spring';
+      break;
+    case (month >=5 && month <= 7):
+      monthName = 'summer'
+      break;
+    case (month >=8 && month <= 10):
+      monthName = 'autumn';
+      break;
+    case (month >= 11 || month <= 1):
+      monthName = "winter";
+      break;
+  }
+
+  return monthName;
 };
