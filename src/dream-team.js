5084de86d438398a81e2cc9e256ac7e5bebe89e0
ortophius@mail.ru <ortophius@mail.ru>
2020-09-18 02:39:59 +0400

    feat: implemented dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false;
+  let teamName = '';
+
+  members.forEach(name => {
+    if (typeof name !== 'string') return;
+    if (name.length === 0) return;
+  
+    name = name.trim()
+
+    if (!(/^[A-Za-z]$/.test(name[0]))) return;
+    teamName += name[0].toUpperCase();
+  });
+
+  return teamName.split('').sort().join('');
 };
